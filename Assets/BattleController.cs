5564191b1aee9fd2eabc03b43b25d98bfeab11e2
yassinawad1 <98675346+yassinawad1@users.noreply.github.com>
2023-11-05 17:11:20 +0000

    Add files via upload

---
+++ Assets/BattleController.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+public class BattleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject[] heroPrefabs;
+    public GameObject enemyPrefab;
+    [SerializeField]
+    public Transform handUI;
+    public int partySize;
+    public GameObject cardDefault;
+    public static List<Hero> party;
+    public static int turnIndex;
+
+    public static List<Enemy> enemies;
+    public int enemyCount;
+    public static bool turnInProgress = false;
+    public TMP_Text roundCounter;
+
+    public GameObject deckButton;
+    public GameObject discardButton;
+    void Start()
+    {   
+        randomizeTurnOrder();
+        party = new List<Hero>();
+        enemies = new List<Enemy>();
+        populateSides();
+        StartCoroutine(Combat());
+       
+    }
+    IEnumerator Combat(){
+        int RoundCount=0;
+        bool finished = false;
+        while(!finished){
+            roundCounter.text = string.Format("Round: {0}",++RoundCount);
+            setEnemyIntention();
+            foreach(Hero hero in party.ToList()){ 
+                hero.toggleActive();
+                hero.resolveStatuses();
+                hero.reduceStatuses();
+                if(hero.hand.Count == 0){
+                    drawCards();
+                }else{
+                    reloadCards();
+                }
+                deckButton.transform.GetChild(0).GetComponent<TMP_Text>().text = string.Format("Deck: {0}",hero.currentDeck.Count);
+                discardButton.transform.GetChild(0).GetComponent<TMP_Text>().text = string.Format("Discard: {0}",hero.discardPile.Count);
+                yield return new WaitUntil(() => turnInProgress);
+                turnInProgress = false;
+                if(enemies.Count ==0){
+                    finished = true;
+                    break;
+                }
+                hero.toggleActive();
+                nextTurn();
+            }
+            if(!finished){
+                foreach(Enemy enemy in enemies.ToList()){
+                    enemy.resolveStatuses();
+                    if(!enemy.isDead){
+                        enemy.takeTurn();
+                        enemy.reduceStatuses();
+                        if(party.Count ==0){
+                            finished = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        //CombatOver here;
+        if(enemies.Count ==0){
+            Debug.Log("Combat won");
+            //func for Rewards
+        }else{
+            Debug.Log("Game over");
+        }
+    }
+    void nextTurn(){
+        turnIndex = (turnIndex + 1) % partySize;
+        reloadCards();
+    }
+    private void populateSides(){
+        Vector3 StartPos = new Vector3(-1,1,10);
+        Vector3 offset =new Vector3(-11,0,-12);
+
+        for(int i = 1; i < partySize+1;i++){
+            GameObject obj = Instantiate<GameObject>(heroPrefabs[i-1],transform.position,transform.rotation);
+            obj.transform.position  = StartPos + i*(offset/(partySize+1));
+            Hero hScript = obj.GetComponent<Hero>();
+            party.Add(hScript);
+        }
+        for(int i=1;i<enemyCount +1;i++){
+            GameObject obj = Instantiate<GameObject>(enemyPrefab,transform.position,transform.rotation);
+            obj.transform.position=Vector3.Reflect(StartPos + i*(offset/(enemyCount+1)),Vector3.right);
+            Enemy eScript = obj.GetComponent<Enemy>();
+            enemies.Add(eScript);
+        }
+    }
+
+    private void randomizeTurnOrder(){
+        for (int i = heroPrefabs.Length - 1; i > 0; i--){
+            var k = Random.Range(0,i+1);
+            var value = heroPrefabs[k];
+            heroPrefabs[k] = heroPrefabs[i];
+            heroPrefabs[i] = value;
+        }
+    }
+
+    private void setEnemyIntention(){
+        foreach(Enemy enemy in enemies){
+            enemy.setIntention();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void drawCards(){
+        for(int i = 0; i <5; i++){
+            Card card = Instantiate(cardDefault).GetComponent<Card>();
+            card.transform.SetParent(handUI,false);
+            card.ID = party[turnIndex].draw();
+        }
+    }
+    void reloadCards(){
+        for(int i =0; i < handUI.childCount;i++){
+            handUI.GetChild(i).GetComponent<Card>().destroy();
+        }
+        foreach(int id in party[turnIndex].hand){
+            Card card = Instantiate(cardDefault).GetComponent<Card>();
+            card.transform.SetParent(handUI,false);
+            card.ID = id;
+        }
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Enemy : MonoBehaviour, IcombatFunction{
+    
+    public StatusCounter statusCounter;
+    public HPBar hpBar;
+
+    public TMP_Text intentText;
+    
+    public GameObject statusBar;
+    [SerializeField]
+    public int maxHP = 60;
+    [SerializeField]
+    private int currentHP;
+
+    public int shield = 0;
+    public Dictionary<string,int> statuses = new Dictionary<string, int>();
+    public bool isDead;
+    private string[] canApply = new string[]{"weakened","vulnerable"};
+    int intent;
+    int targetInd;
+
+    string intentValue;
+//intent who for what 
+    // Start is called before the first frame update
+    void Awake(){
+        currentHP = maxHP;
+        hpBar.setMax(maxHP);
+    }
+    void start(){
+
+    }
+    public void setIntention(){
+        int choiceVal = Random.Range(0,100);
+        targetInd = Random.Range(0,BattleController.party.Count);
+        if(choiceVal <=50){
+            intent = 0; // attack
+            intentValue = Random.Range(6,11).ToString();
+            intentText.text = string.Format("Attacking {0} for {1}",BattleController.party[targetInd].name, intentValue);
+        }else if(choiceVal <=80){
+            intent = 1; //block
+            intentValue = Random.Range(6,11).ToString();
+            intentText.text = string.Format("Blocking for {0}", intentValue);
+        }else{
+            intent = 2;//status effect
+            intentValue = canApply[Random.Range(0,2)];
+            intentText.text = string.Format("Applying {0} on {1}",intentValue,BattleController.party[targetInd].name);
+        }
+    }
+
+    public void takeTurn(){
+        targetInd = targetInd % BattleController.party.Count;
+        if(intent == 0){
+            int dmg = statuses.ContainsKey("weakened")? int.Parse(intentValue)/2 : int.Parse(intentValue);
+            dmg = statuses.ContainsKey("weakened")? dmg*2 : dmg;
+            BattleController.party[targetInd].getHit(statuses.ContainsKey("weakened")? int.Parse(intentValue)/2 : int.Parse(intentValue),false);
+        }else if(intent == 1){
+            defend(int.Parse(intentValue));
+        }else if(intent == 2){
+            BattleController.party[targetInd].applyStatus(intentValue,2);
+        }
+    }
+    public void applyStatus(string status, int amount){
+        try{
+            statuses[status] += amount;
+            StatusCounter s = statusBar.transform.Find(status).GetComponent<StatusCounter>();
+            s.updateCount(amount);
+        }catch(KeyNotFoundException){
+            statuses.Add(status,amount);
+            StatusCounter s = Instantiate(statusCounter).GetComponent<StatusCounter>();
+            s.transform.SetParent(statusBar.transform,false);
+            s.statusType = status;
+            s.updateCount(amount);
+        }
+        if (status == "weakened" & intent ==0){
+            intentText.text = string.Format("Attacking {0} for {1}",BattleController.party[targetInd].name,int.Parse(intentValue)/2);
+        }
+    }
+    public void reduceStatuses(){
+        List<string> keys = new List<string>(statuses.Keys);
+        foreach(string key in keys){
+            statuses[key] -=1;
+            if(statuses[key] ==0){
+                statuses.Remove(key);
+            }
+        }
+        for(int i=0; i<statusBar.transform.childCount; i++){
+            StatusCounter s = statusBar.transform.GetChild(i).GetComponent<StatusCounter>();
+            s.updateCount(-1);
+        }
+    }
+    public void resolveStatuses(){
+        if(statuses.ContainsKey("poison")){
+            getHit(statuses["poison"],true);
+        }
+        if(!statuses.ContainsKey("barricade")){
+            shield = 0;
+            hpBar.setShield(0);
+        }
+    }
+    public void heal(int amount){
+        currentHP = Mathf.Clamp(currentHP+amount,0,maxHP);
+        hpBar.setHealth(currentHP);
+    }
+    public void getHit(int amount, bool ignoreShield){
+        if(ignoreShield){
+            currentHP -= amount;
+        }else{
+            shield -= (statuses.ContainsKey("vulnerable")) ? amount*2 : amount;
+            if(shield < 0){
+                currentHP +=shield;
+                shield = 0;
+                hpBar.setShield(0);
+        }
+        }
+        if(currentHP <= 0){
+            currentHP=0;
+            die();
+        }
+        hpBar.setHealth(currentHP);
+    }
+
+    public void defend(int amount){
+        shield += amount;
+        hpBar.setShield(shield);
+    }
+    public void die(){
+        BattleController.enemies.Remove(this);
+        transform.eulerAngles = new Vector3(0,0,90);
+        transform.GetChild(0).gameObject.SetActive(false);
+        isDead = true;
+    }
+}
+
